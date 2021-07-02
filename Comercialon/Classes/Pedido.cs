953f80e3795184e3cc70383545285b0ac5cfa824
LUCCA ACETE DE CARVALHO <lucca.acarvalho@senacsp.edu.br>
2021-07-02 11:43:02 -0300

    Piroo

--- Comercialon/Classes/Pedido.cs
+++ Comercialon/Classes/Pedido.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace Comercialon.Classes
 {
-    class Pedido
+    public class Pedido
     {
+        public int Id { get; set; }
+        public int Data { get; set; }
+        public string Situacao { get; set; }
+        public double Desconto { get; set; }
+        public Usuario Usuario { get; set; }
+        public Cliente Cliente { get; set; }
+        public List<ItemPedido> ItemsPedidos { get; set; }
+
+        public Pedido()
+        {
+        }
+
+        public Pedido(int data, string situacao, double desconto, Usuario usuario, Cliente cliente, List<ItemPedido> itemsPedidos)
+        {
+            Data = data;
+            Situacao = situacao;
+            Desconto = desconto;
+            Usuario = usuario;
+            Cliente = cliente;
+            ItemsPedidos = itemsPedidos;
+        }
+
+        public Pedido(int id, int data, string situacao, double desconto, Usuario usuario, Cliente cliente, List<ItemPedido> itemsPedidos)
+        {
+            Id = id;
+            Data = data;
+            Situacao = situacao;
+            Desconto = desconto;
+            Usuario = usuario;
+            Cliente = cliente;
+            ItemsPedidos = itemsPedidos;
+        }
     }
 }
--- Comercialon/Classes/Produto.cs
+++ Comercialon/Classes/Produto.cs
@@ -7,32 +7,67 @@ using System.Threading.Tasks;
 namespace Comercialon.Classes
 {
    public class Produto
-    {
+    { 
        public string Id { get; set; }
        public string Descrição { get; set; }
-       public double Preço { get; set; }
+       public double ValorUnitario { get; set; }
+       public string UnidadeVenda { get; set; }
+       public string CodBar { get; set; }
        public double Desconto { get; set; }
        public bool Descontinuando { get; set; }
+       public Marca Marca { get; set; }
+       public Categoria Categoria { get; set; }
 
         public Produto()
         {
         }
 
-        public Produto(string id, string descrição, double preço, double desconto, bool descontinuando)
+        public Produto(string descrição, double valorUnitario, string unidadeVenda, string codBar, double desconto, bool descontinuando, Marca marca, Categoria categoria)
+        {
+            Descrição = descrição;
+            ValorUnitario = valorUnitario;
+            UnidadeVenda = unidadeVenda;
+            CodBar = codBar;
+            Desconto = desconto;
+            Descontinuando = descontinuando;
+            Marca = marca;
+            Categoria = categoria;
+        }
+
+        public Produto(string id, string descrição, double valorUnitario, string unidadeVenda, string codBar, double desconto, bool descontinuando, Marca marca, Categoria categoria)
         {
             Id = id;
             Descrição = descrição;
-            Preço = preço;
+            ValorUnitario = valorUnitario;
+            UnidadeVenda = unidadeVenda;
+            CodBar = codBar;
             Desconto = desconto;
             Descontinuando = descontinuando;
+            Marca = marca;
+            Categoria = categoria;
         }
 
-        public Produto(string descrição, double preço, double desconto, bool descontinuando)
+        public void inserir()
         {
-            Descrição = descrição;
-            Preço = preço;
-            Desconto = desconto;
-            Descontinuando = descontinuando;
+
+        }
+        public static List<Produto> ObterLista()
+        {
+            List<Produto> lista = new List<Produto>();
+
+            return lista;
+        }
+        public void BuscarPorId(int id)
+        {
+
+        }
+        public void BuscarPorCodbar(string codbar)
+        {
+
+        }
+        public bool Alterar()
+        {
+            return true;
         }
     }
 }
